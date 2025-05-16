904cff8d25a0bf7f976a7db6287b72de49c768fb
Prakruti_Tops <109515514+prakrutitops@users.noreply.github.com>
2025-05-16 15:01:03 +0530

    commit

---
+++ 16-5/main (43).c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+#include<conio.h>
+
+int main()
+{
+    // printf("\n hello world");
+    // printf("\n shubham");
+    // printf("\n mandeep");
+    
+     printf("\n hello world");
+    printf("\t shubham");
+    printf("\t mandeep");
+    
+    
+    return 0;    
+}
---
+++ 16-5/main (45).c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+#include<conio.h>
+
+void main()
+{
+    int a = 5;
+    float b = 56.78;
+    char c = 'S';
+    double d = 56.874568;
+    
+    printf("\n value is : %d",a);
+    printf("\n value is : %f",b);
+    printf("\n value is : %c",c);
+    printf("\n value is : %lf",d);
+    
+    getch();
+}
