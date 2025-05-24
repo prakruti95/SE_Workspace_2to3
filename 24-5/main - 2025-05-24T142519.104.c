358396ff1045c5e78d36f27031ec642e83f71ae9
Prakruti_Tops <109515514+prakrutitops@users.noreply.github.com>
2025-05-24 15:01:25 +0530

    commit

---
+++ 24-5/main - 2025-05-24T142519.104.c
@@ -0,0 +1,23 @@
+#include<stdio.h>
+#include<conio.h>
+int main()
+{
+    
+    int i,j,num;
+    
+    printf("Enter Value: ");
+    scanf("%d",&num);
+    
+    for(i=0;i<=num;i++)
+    {
+        for(j=0;j<=i;j++)
+        {
+            char ch = 'A'+i;
+            printf("%c",ch);
+        }
+        printf("\n");
+    }
+    
+    
+    return 0;
+}
