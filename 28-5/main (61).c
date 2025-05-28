f194e48a39a9e8d9bde6422776a74b61c7178ab8
Prakruti_Tops <109515514+prakrutitops@users.noreply.github.com>
2025-05-28 14:57:14 +0530

    commit

---
+++ 28-5/main (61).c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+
+int main()
+{
+     printf("\n %f",ceil(3.6)); 
+     printf("\n %f",ceil(3.4)); 
+     printf("\n %f",floor(3.6));  
+     printf("\n %f",floor(3.4));  
+     printf("\n %f",sqrt(25));     
+     printf("\n %f",sqrt(7)); 
+     printf("\n %f",pow(2,4)); 
+     printf("\n %f",pow(2,5)); 
+     printf("\n %d",abs(-12));  
+    return 0;
+}
