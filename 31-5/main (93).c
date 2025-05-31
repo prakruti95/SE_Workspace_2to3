9be27b2bfc54bd06741c2e17d366c4bf92168681
Prakruti_Tops <109515514+prakrutitops@users.noreply.github.com>
2025-05-31 16:26:11 +0530

    commit

---
+++ 31-5/main (93).c
@@ -0,0 +1,35 @@
+#include <stdio.h>
+#include <conio.h>
+
+void main()
+{
+	int row,colom,space,loop;
+
+
+	printf("Enter the Row for the Pyramid : ");
+	scanf("%d",&row);
+
+		for (loop=1;loop <=row;loop++)
+	{
+		for (space=0;space<row-loop+2;space++)
+			printf(" ");
+
+		for (colom=1; colom <= loop; colom++)
+			printf("* ");
+
+		printf("\n");
+	}
+
+	for (loop=row;loop>=1;loop--)
+	{
+		for (space=0;space<row-loop+2;space++)
+			printf(" ");
+
+		for (colom=1; colom <= loop; colom++)
+			printf("* ");
+
+		printf("\n");
+	}
+
+	getch();
+}
