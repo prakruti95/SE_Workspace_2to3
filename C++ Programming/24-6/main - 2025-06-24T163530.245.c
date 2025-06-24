489f10d9c951d3d810ccaa0a0e03f7b50b231984
Prakruti_Tops <109515514+prakrutitops@users.noreply.github.com>
2025-06-24 17:03:15 +0530

    commit

---
+++ C++ Programming/24-6/main - 2025-06-24T163530.245.c
@@ -0,0 +1,21 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+    int i,j,k;
+    
+    for(i=1;i<=5;i++)
+    {
+      for(j=1;j<=(5-i);j++)
+      {
+          printf(" ");
+      }
+       for(k=i;k>=1;k--)
+       {
+            printf("%d",k);
+       }
+        printf("\n");
+    }
+    
+    getch();
+}
